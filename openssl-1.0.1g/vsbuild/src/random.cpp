a24f5f9465ef9ecf11f265356b96288608a84b9e
Valeriy Kopylov <valery.kopylov@akvelon.com>
2014-04-24 11:03:59 +0400

    WP8 platform related changes
    Random numbers generator now is seeded using std::random_device class
    instead of other not available on WP8 sources of entropy.

---
+++ openssl-1.0.1g/vsbuild/src/random.cpp
@@ -0,0 +1,14 @@
+﻿#include "cryptlib.h"
+
+#if (defined(OPENSSL_SYS_WINDOWS) || defined(OPENSSL_SYS_WIN32)) && _WIN32_WINNT==0x0602
+
+#include "random.h"
+#include <random>
+
+int GetRandom()
+{
+    static std::random_device engine;
+    return engine();
+}
+
+#endif
